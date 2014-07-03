e13d7db8799e8f4817b299563cee0e15fb563158
C.J. Jameson <cjcjameson@gmail.com>
2014-07-03 12:14:45 -0700

    Rails and Testing Setup
     - RSpec and Jasmine: gems, installed, and in spec_helper
     - Database .yml

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Amber::Application.config.secret_token = 'd9b6968c281aab18e93dd0d18ee2c94390a82308db2198c4eb1593072901b19f940b40d993c16b374793617f9c299189af03bb7fe24a678e35bda4f4b88d75bb'
