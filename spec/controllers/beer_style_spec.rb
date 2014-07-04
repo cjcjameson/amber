0c73d95ea6ba69039ea725df939633b52fec1232
C.J. Jameson <cjcjameson@gmail.com>
2014-07-04 11:56:55 -0700

    RSpec Tests for search routes / controllers
     - Added very basic erb view placeholders because that was causing a bug
     - Pending tests because the next big step is to rename beer_style to beer_genre
     				and beer_search_term to beer_style

---
+++ spec/controllers/beer_style_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe BeerStylesController do
+	context "#search" do
+		it "gets food flavors in" do
+			get :search, {flavor1: "salty", flavor2: "sweet"}
+			expect(assigns(:food_flavors).keys).to include("flavor1")
+		end
+
+		it "returns an error if not provided any search terms" do
+			pending "wait until we rename the styles and genres"
+		end
+
+		it  "returns an error if search terms are not flavors" do
+			pending "wait until we rename the styles and genres"
+		end
+	end
+end
