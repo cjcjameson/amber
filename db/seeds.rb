ef90ba57df7bfafac6298ea0eadac6d1103682b8
allison <Allison@bbswag.local>
2014-07-03 20:11:21 -0700

    created seedfile base

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+for i in 1..5
+  result = HTTParty.get("http://api.brewerydb.com/v2/beers/?key=ecca795782bac08b48d8d9aac51602c8&p#{i}&status=verified&format=json")
+  parsed_results = JSON.parse(result.to_json)["data"]
+  parsed_results.map { |result| Beer.create(name: result["name"], description: result["description"], abv: result["abv"], isOrganic: result["isOrganic"], status: result["status"], available: result["available"]["name"], category: result["style"]["category"]["name"]), style: result["style"]["name"] }
+end
