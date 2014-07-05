75d3615c0871e353b6971b5c8217e6191be20c4f
rubberyuzu <yuzu0125@gmail.com>
2014-07-05 15:04:06 -0700

    added routing logic for genres search

--- app/controllers/beer_genres_controller.rb
+++ app/controllers/beer_genres_controller.rb
@@ -4,6 +4,14 @@ class BeerGenresController < ApplicationController
 		if flavor_keys.length == 0
 			render file: "public/422", status: 422
 		end
-		@food_flavors = flavor_keys.map { |f| params[f] }
+		result = Hash.new
+		flavor_keys.each do |flavor|
+			genres = FoodFlavor.find_by_name(flavor).beer_genres
+			genres.each do |genre|
+				result[genre] ? result[genre] += 1 : result[genre] = 1
+			end
+		end
+		result = result.sort_by{|k,v| v}.reverse #=> this gives you the nested array. [[name: count],[name:count]]
+		@beer_genres = result.map{ |genre_arr| genre_arr.first.to_json } #=> this gives you the array of the genres in JSON, sorted by the occurence.
 	end
 end
