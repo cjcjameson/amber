f14bcd8ef8309e507545ae9054c8976703626b28
C.J. Jameson <cjcjameson@gmail.com>
2014-07-05 15:40:53 -0700

    Updated genre search controller test
     - New before block to set up test environment
     - refinement of json output
     - clean up p statements

--- app/controllers/beer_genres_controller.rb
+++ app/controllers/beer_genres_controller.rb
@@ -5,13 +5,13 @@ class BeerGenresController < ApplicationController
 			render file: "public/422", status: 422
 		end
 		result = Hash.new
-		flavor_keys.each do |flavor|
-			genres = FoodFlavor.find_by_name(flavor).beer_genres
+		flavor_keys.each do |flavor_key|
+			genres = FoodFlavor.find_by_name(params[flavor_key]).beer_genres
 			genres.each do |genre|
 				result[genre] ? result[genre] += 1 : result[genre] = 1
 			end
 		end
 		result = result.sort_by{|k,v| v}.reverse #=> this gives you the nested array. [[name: count],[name:count]]
-		@beer_genres = result.map{ |genre_arr| genre_arr.first.to_json } #=> this gives you the array of the genres in JSON, sorted by the occurence.
+		@beer_genres = result.map{ |genre_arr| genre_arr.first }.to_json #=> this gives you the array of the genres in JSON, sorted by the occurence.
 	end
 end
--- spec/controllers/beer_genre_spec.rb
+++ spec/controllers/beer_genre_spec.rb
@@ -2,9 +2,15 @@ require 'spec_helper'
 
 describe BeerGenresController do
 	context "#search" do
+		before (:each) do
+			spicy = FoodFlavor.create(name: "spicy")
+			ipa = BeerGenre.create(name: "IPA")
+			match = Match.create(food_flavor: spicy, beer_genre: ipa)
+		end
+
 		it "gets food flavors in" do
-			get :search, {flavor1: "salty", flavor2: "sweet"}
-			expect(assigns(:food_flavors)).to include("salty")
+			get :search, {flavor1: "spicy"}
+			expect(assigns(:beer_genres)).to include("IPA")
 		end
 
 		it "returns an error if not provided any flavorful search terms" do
