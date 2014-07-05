604b11e11fa11e5d9d82e48a254d083de489903b
rubberyuzu <yuzu0125@gmail.com>
2014-07-04 20:30:40 -0700

    added a fake seed method for beer table

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,29 +10,50 @@ class Seed
     end
   end
 
-  def self.beers(num)
-    for i in 1..num
-      puts i
-      result = HTTParty.get("http://api.brewerydb.com/v2/beers/?key=ecca795782bac08b48d8d9aac51602c8&p#{i}&status=verified&format=json")
-      parsed_results = JSON.parse(result.to_json)["data"]
-      parsed_results.each do |result|
-        new_beer = Beer.create(name: result["name"],
-         description: result["description"],
-         abv: result["abv"], 
-         isOrganic: result["isOrganic"],
-         status: result["status"])
+  # def self.beers(num)
+  #   for i in 1..num
+  #     puts i
+  #     result = HTTParty.get("http://api.brewerydb.com/v2/beers/?key=ecca795782bac08b48d8d9aac51602c8&p#{i}&status=verified&format=json")
+  #     parsed_results = JSON.parse(result.to_json)["data"]
+  #     parsed_results.each do |result|
+  #       new_beer = Beer.create(name: result["name"],
+  #        description: result["description"],
+  #        abv: result["abv"], 
+  #        isOrganic: result["isOrganic"],
+  #        status: result["status"])
         
-        if result["available"]
-        	new_beer.update_attributes(available: result["available"]["name"])
-        end
-        if result["style"]
-        	new_beer.update_attributes(category: result["style"]["category"]["name"],
-            style: result["style"]["name"])
-        end
-        if result["labels"]
-          new_beer.update_attributes(icon_url: result["labels"]["icon"])
-        end
-      end
+  #       if result["available"]
+  #       	new_beer.update_attributes(available: result["available"]["name"])
+  #       end
+  #       if result["style"]
+  #       	new_beer.update_attributes(category: result["style"]["category"]["name"],
+  #           style: result["style"]["name"])
+  #       end
+  #       if result["labels"]
+  #         new_beer.update_attributes(icon_url: result["labels"]["icon"])
+  #       end
+  #     end
+  #   end
+  # end
+  def self.beers_fake
+    names = [".38 Special", "100 Barrel Series #31", "13 Rebels",
+     "1327 Pod", "Red Mullet Ale", "Stone Jail 13",
+      "Ziegenbock Amber", "Yosemite Scooter", "Wupper Hell",
+      "Wusele Weizen", "Belmont Station 15th Anniversary Ale",
+      "Below Decks", "Cello Stout", "Celt's Golden Ale", 
+      "Celtic Ale", "Eighty Shilling Export Ale", "Eighty-Four",
+      "Eighty-Shilling Scottish Ale (80/-)", "Einbecker Alster",
+      "Einbecker Brauherren Alkoholfrei"]
+    categories = ["Bitter", "Pilsner", "Porter",
+     "Cream Ale / Blonde Ale"]
+    styles = ["Ordinary Bitter", "German-Style Pilsener",
+     "Brown Porter", "Golden or Blonde Ale"]
+    abv = [3.8, 5.8, 5.2, 5.6]
+    available = ["Year Round", "Seasonal"]
+    icon_url = "https://s3.amazonaws.com/brewerydbapi/beer/FOQMff/upload_3aTloe-icon.png"
+    description = "It's good!"
+    names.each_with_index do |name, index|
+      Beer.create(name: name, abv: abv[index % 4], available: available[index % 2], category: categories[index % 4], style: styles[index % 4], icon_url: icon_url, description: description)
     end
   end
 
@@ -48,5 +69,6 @@ class Seed
 end
 
 Seed.genres
-Seed.beers(5) #change the input to 581 when actually seeding
+# Seed.beers(5) #change the input to 581 when actually seeding
 Seed.flavors
+Seed.beers_fake #for ourselves, fake database
