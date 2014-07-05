d03e356d1dc4c67c19f1935521de7f7556d8049a
rubberyuzu <yuzu0125@gmail.com>
2014-07-04 17:53:16 -0700

    clean up the seedfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,44 +1,52 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-# #   Mayor.create(name: 'Emanuel', city: cities.first)
-beer_genres = ["Cream Ale / Blonde Ale", "Bitter", "Pale Ale", "IPA", "Double IPA", "Amber Ale / Red Ale", "Scotch Ale", "Brown Ale / Altbier", "Imperial Stout", "Oatmeal Stout", "Old Ale / Strong Ale", "Porter", "Abbey Dubbel", "Abbey Tripel", "Hefewiezen", "Witbier", "American Wheat", "Barley Wine"]
-
-beer_genres.each do |genre|
-  BeerGenre.create(name: genre)
-end
-
-for i in 1..5
-  puts i
-  result = HTTParty.get("http://api.brewerydb.com/v2/beers/?key=ecca795782bac08b48d8d9aac51602c8&p#{i}&status=verified&format=json")
-  parsed_results = JSON.parse(result.to_json)["data"]
-  parsed_results.each do |result|
-    new_beer = Beer.create(name: result["name"],
-     description: result["description"],
-     abv: result["abv"], 
-     isOrganic: result["isOrganic"],
-     status: result["status"])
-
-    if result["available"]
-    	new_beer.update_attributes(available: result["available"]["name"])
+class Seed
+  def self.genres 
+    beer_genres = ["Cream Ale / Blonde Ale", "Bitter", "Pale Ale", "IPA",
+     "Double IPA", "Amber Ale / Red Ale", "Scotch Ale", 
+     "Brown Ale / Altbier", "Imperial Stout", "Oatmeal Stout", 
+     "Old Ale / Strong Ale", "Porter", "Abbey Dubbel", "Abbey Tripel",
+      "Hefewiezen", "Witbier", "American Wheat", "Barley Wine"]
+    beer_genres.each do |genre|
+      BeerGenre.create(name: genre)
     end
-    if result["style"]
-    	new_beer.update_attributes(category: result["style"]["category"]["name"],style: result["style"]["name"])
+  end
+
+  def self.beers(num)
+    for i in 1..num
+      puts i
+      result = HTTParty.get("http://api.brewerydb.com/v2/beers/?key=ecca795782bac08b48d8d9aac51602c8&p#{i}&status=verified&format=json")
+      parsed_results = JSON.parse(result.to_json)["data"]
+      parsed_results.each do |result|
+        new_beer = Beer.create(name: result["name"],
+         description: result["description"],
+         abv: result["abv"], 
+         isOrganic: result["isOrganic"],
+         status: result["status"])
+        
+        if result["available"]
+        	new_beer.update_attributes(available: result["available"]["name"])
+        end
+        if result["style"]
+        	new_beer.update_attributes(category: result["style"]["category"]["name"],
+            style: result["style"]["name"])
+        end
+        if result["labels"]
+          new_beer.update_attributes(icon_url: result["labels"]["icon"])
+        end
+      end
     end
-    if result["labels"]
-      new_beer.update_attributes(icon_url: result["labels"]["icon"])
+  end
+
+  def self.flavors
+    flavors = ["strong","bitter","fried/fatty","spicy","salty",
+      "floral","light","citrus/acidic","sweet","spiced","creamy",
+      "rich","earthy","nutty","gamey","savory","grilled","smokey",
+      "heavy","hearty"]
+    flavors.each do |f|
+      FoodFlavor.create(name: f)
     end
   end
 end
 
-#==========================
-#==========YUZU============
-
-flavors = ["strong","bitter","fried/fatty","spicy","salty","floral","light","citrus/acidic","sweet","spiced","creamy","rich","earthy","nutty","gamey","savory","grilled","smokey","heavy","hearty"]
-flavors.each do |f|
-  FoodFlavor.create(name: f)
-end
-#==========================
+Seed.genres
+Seed.beers(5) #change the input to 581 when actually seeding
+Seed.flavors
