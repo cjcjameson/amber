c63d5cadf593e5179ef07d6bc7e56ad666e96d97
djohnson0610 <djohnson0610@hotmail.com>
2014-07-06 00:08:34 -0700

    get genres form recieving data via ajax

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,1 +1,21 @@
-// text goes here
+$(document).ready(function(){
+	bindEvents()
+})
+
+function bindEvents(){
+	$('#search_genres').on('submit', getGenres)
+}
+
+function getGenres(){
+	event.preventDefault()
+	var getData = $(this).serializeArray();
+	$.ajax({
+		url: 'beer_genres/search',
+		method: 'GET',
+		data: getData
+	}).success(function(){
+		console.log("yay!")
+	}).fail(function(){
+		console.log("boo!")
+	})
+}
