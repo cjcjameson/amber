86ce73cbaf9ab03c37f583c952361caf46453f93
C.J. Jameson <cjcjameson@gmail.com>
2014-07-05 17:24:35 -0700

    More controller testing for 'food_flavors#search'
    This is helping to refactor our controller and confirm our JSON structure

--- app/controllers/beer_genres_controller.rb
+++ app/controllers/beer_genres_controller.rb
@@ -1,17 +1,19 @@
 class BeerGenresController < ApplicationController
 	def search
-		flavor_keys = params.keys.select { |k| k =~ /flavor/ }
-		if flavor_keys.length == 0
+		flavors = params.select { |key,flavor| key =~ /flavor/ }.values
+		if flavors.length == 0
 			render file: "public/422", status: 422
+			return
 		end
 		result = Hash.new
-		flavor_keys.each do |flavor_key|
-			genres = FoodFlavor.find_by_name(params[flavor_key]).beer_genres
+		flavors.each do |flavor|
+			genres = FoodFlavor.find_by_name(flavor).beer_genres
 			genres.each do |genre|
 				result[genre] ? result[genre] += 1 : result[genre] = 1
 			end
 		end
 		result = result.sort_by{|k,v| v}.reverse #=> this gives you the nested array. [[name: count],[name:count]]
-		@beer_genres = result.map{ |genre_arr| genre_arr.first }.to_json #=> this gives you the array of the genres in JSON, sorted by the occurence.
+		@beer_genres = result.map{ |genre_arr| genre_arr.first } #=> this gives you the array of the genres in JSON, sorted by the occurence.
+		render json: @beer_genres
 	end
 end
--- spec/controllers/beer_genre_spec.rb
+++ spec/controllers/beer_genre_spec.rb
@@ -8,9 +8,22 @@ describe BeerGenresController do
 			match = Match.create(food_flavor: spicy, beer_genre: ipa)
 		end
 
-		it "gets food flavors in" do
+		it "prepares a beer genre" do
 			get :search, {flavor1: "spicy"}
-			expect(assigns(:beer_genres)).to include("IPA")
+			expect(assigns(:beer_genres).first.name).to include("IPA")
+		end
+
+		it "returns successfully" do
+			get :search, {flavor1: "spicy"}
+			expect(response).to be_success
+		end
+
+		it "returns JSON recommending IPA" do
+			get :search, {flavor1: "spicy"}
+			beer_genres = JSON.parse(response.body)
+			# p beer_genres
+			# names = beer_genres.map{ |g| g.name }
+			expect(beer_genres.to_s).to include("IPA")
 		end
 
 		it "returns an error if not provided any flavorful search terms" do
