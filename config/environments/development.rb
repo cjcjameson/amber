abcc23070bccb6907fdd09b85374c137b3198c6f
djohnson0610 <djohnson0610@hotmail.com>
2014-07-07 08:00:10 -0700

    figured out custom fonts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,5 +34,4 @@ Amber::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
-  config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
 end
