35c1ec487de37c1645c7640c74fd924d15b10c2e
djohnson0610 <djohnson0610@hotmail.com>
2014-07-07 00:27:25 -0700

    beers showing more information

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -29,25 +29,38 @@ function getBeers(){
 
 function displayGenreMatches(matchArray){
 	$('#search_results').empty()
-	
+	$('#search_results').append('<div id="genre_results"></div>')
 	for (var match = 0; match < matchArray.length; match ++){
 		var name = matchArray[match].name
 		var description = matchArray[match].description
 		var genre = $('#genre_template').children().clone()
-		genre.addClass(name)
+		// genre.addClass(name)
 		genre.append("<h4 class='centered_text'>"+name+"</h4>")
 		genre.append('<p class="small_side_margins">'+description+'</p>')
 		genre.append("<button id='"+name+"' class='beer_search absolute'>Get Suggestions!</button>")
-		$('#search_results').append(genre)
+		$('#genre_results').append(genre)
 	}
+	$('#genre_results').append('<div id="beer_results" class="clear_floats">')
 }
 
 
 function displayBeers(data){
-	$('.beer_list').empty();
+	$('#beer_results').empty();
 	var beers = data.beers
-	debugger
-	for (var i=0; i<beers.length; i++){
-		$('.beer_list').append('<li>'+ beers[i].name +'</li>')
+	// debugger
+	// :name, :description, :abv, :available, :category, :style, :icon_url
+	for (var sample = 0; sample < beers.length; sample++){
+		var sampleBeer = $('#beer_template').children().clone()
+		var name = beers[sample].name
+		var description = beers[sample].description
+		var abv = beers[sample].abv
+		var style = beers[sample].style
+		var imgUrl = beers[sample].icon_url
+		sampleBeer.append("<h4>"+name+"</h4>")
+		sampleBeer.append("<h6>"+style+"</h6>")
+		sampleBeer.append("<p>"+description+"</p>")
+		sampleBeer.append("<img src='"+imgUrl+"'>")
+		$('#beer_results').append(sampleBeer)
+		// $('.beer_list').append('<li>'+ beers[i].name +'</li>')
 	}
 }
