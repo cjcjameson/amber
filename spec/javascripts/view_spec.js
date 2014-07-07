5fef88393800dd3419ff1394f5023758bc9c7f96
allison <Allison@bbswag.local>
2014-07-07 16:17:20 -0700

    mock ajax added and initial test working

--- spec/javascripts/view_spec.js
+++ spec/javascripts/view_spec.js
@@ -3,3 +3,37 @@ describe("homepage", function() {
 		expect(true).toBe(true);
 	});
 });
+
+describe("suite wide mocking ajax", function() {
+
+  describe("homepage", function() {
+
+    beforeEach(function() {
+      jasmine.Ajax.install();
+    });
+
+    afterEach(function() {
+      jasmine.Ajax.uninstall();
+    });
+
+    it("displays an okay response for getBeers", function(){
+      var doneFn = jasmine.createSpy("success");
+      var xhr = new XMLHttpRequest();
+      xhr.onreadystatechange = function(args) {
+        if (this.readyState == this.DONE) {
+          doneFn(this.responseText);
+        }
+      };
+      xhr.open("GET", "beers/search");
+      xhr.send();
+
+      expect(jasmine.Ajax.requests.mostRecent().url).toBe("beers/search");
+      expect(doneFn).not.toHaveBeenCalled();
+      jasmine.Ajax.requests.mostRecent().response({
+        "status": 200,
+        "contentType": 'json',
+        "responseText": 'whatever'
+      })
+    });
+  });
+});
