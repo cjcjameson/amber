d924535d1b0331b944d6aacf6743cbe09e994d92
C.J. Jameson & Devin Johnson <pair+cjcjameson.djohnson0610@devbootcamp.com>
2014-07-07 21:35:59 -0700

    Refactored the beer_genres_controller
     - Main logic put into GenreCalculator
     - Form now passes flavors in array flavors[]
     - Revised tests to reflect updates to data coming in an array
     - Small refactoring in response sections

--- app/controllers/beer_genres_controller.rb
+++ app/controllers/beer_genres_controller.rb
@@ -1,28 +1,15 @@
 class BeerGenresController < ApplicationController
 	def search
-		flavors = params.select { |key,flavor| key =~ /flavor/ }.values
+		flavors = params[:flavors]
 
-		if flavors.length == 0
-			redirect_to "/public/422", status: 422
-			return
+		unless flavors && flavors.length > 0
+				render text: "please select a flavor profile", status: 422
+				return
 		end
 
-		result = Hash.new
-		flavors.each do |flavor|
-			genres = FoodFlavor.find_by_name(flavor).beer_genres
-			genres.each do |genre|
-				result[genre] ? result[genre] += 1 : result[genre] = 1
-			end
-		end
-		result = result.sort_by{|k,v| v}.reverse #=> this gives you the nested array. [[name: count],[name:count]]
-		result = result[0,4]
-		@beer_genres = result.map{ |genre_arr| genre_arr.first } 
-		
-		respond_to do |format|
-      # format.html
-      format.json { render :json => @beer_genres.to_json }
-    end
+		best_match_genres = GenreCalculator.new(flavors).get_best_genre_matches
 
+		render json: best_match_genres
 	end
 
 	def index
