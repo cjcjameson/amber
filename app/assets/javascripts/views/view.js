b9a8ffd93637ce42ec8d9374647ca57895078dca
rubberyuzu <yuzu0125@gmail.com>
2014-07-09 12:36:46 -0700

    styling beer

--- app/assets/javascripts/views/view.js
+++ app/assets/javascripts/views/view.js
@@ -7,7 +7,7 @@ View.prototype = {
 	},
 	drawBeer: function(beer){
 			var sampleBeer = $('#beer_template').children().clone()
-			sampleBeer.append("<img src='"+beer.label_url+"' class='float_left label_pic two columns'><div class='float_left ten columns'><h3 class='inline'>"+beer.name+"</h3><h5 class='inline'>"+beer.abv+"% - "+beer.style+"</h5><p>"+beer.description+"</p></div>")
+			sampleBeer.append("<img src='"+beer.label_url+"' class='float_left label_pic two columns'><div class='float_left ten columns beer_info'><h3 class='inline'>"+beer.name+"</h3><h5 class='inline'>"+beer.abv+"% - "+beer.style+"</h5><p>"+beer.description+"</p></div>")
 			sampleBeer.addClass('sixteen columns')
 			$("#beer_results").append(sampleBeer)
 	}
