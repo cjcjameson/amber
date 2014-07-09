48cb590b2c6e16d03715526c2e9c50fc4f22297a
djohnson0610 <djohnson0610@hotmail.com>
2014-07-08 17:36:17 -0700

    Beer genre controller specs

--- spec/factories/beer_genres.rb
+++ spec/factories/beer_genres.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :beer_genre do
-
-  	#sub-factory for non-genre
+  	name "Pale Ale"
+  	description "Malty sweetness balanced by hoppy bitterness. Hops bitterness contrasts with spicy, heat-charred, smoky, or aromatic flavors such as those in Stilton and blue cheese."
   end
 end
--- spec/factories/food_flavors.rb
+++ spec/factories/food_flavors.rb
@@ -1,9 +1,6 @@
 
 FactoryGirl.define do
   factory :food_flavor do
-  	#sweet
-  	#salty
-
-  	#sub-factory for non-flavor
+  	name "spicy"
   end
 end
