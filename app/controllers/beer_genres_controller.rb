f115d5d6a62041f2950ddd39224bc3bebc3203c7
allison <azywong@gmail.com>
2014-07-09 15:13:09 -0700

    Handled error where when no params were passed no page would render
    Now it goes back to index with a message

--- app/controllers/beer_genres_controller.rb
+++ app/controllers/beer_genres_controller.rb
@@ -3,7 +3,7 @@ class BeerGenresController < ApplicationController
 		flavors = params[:flavors]
 
 		unless flavors && flavors.length > 0
-				render text: "Please select a flavor profile", status: 422
+				redirect_to root_path, :notice => 'Please select a flavor profile, you donut!'
 				return
 		end
 		@best_match_genres = GenreCalculator.new(flavors).get_best_genre_matches
