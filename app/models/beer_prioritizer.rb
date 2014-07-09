0ba3726ca1c8633493133e827e4e860c443999ab
djohnson0610 <djohnson0610@hotmail.com>
2014-07-09 01:20:59 -0700

    Renamed BeerSorter model and changed Beer Controller to reflect change
    Gave responsibility to find beers to the new model which then sorts them
    based on image availability.

---
+++ app/models/beer_prioritizer.rb
@@ -0,0 +1,19 @@
+class BeerPrioritizer
+  def initialize(genre)
+    @genre = genre
+  end
+
+  def find_beers(@genre)
+    beers = Beer.where('category = ?', @genre)
+    sort_by_image_availability(beers)
+  end
+
+  private
+
+  def sort_by_image_availability(beers)
+    have_image_url = []
+    no_image_url = []
+    beers.each { |beer| beer.label_url ? have_image_url << beer : no_image_url << beer }
+    imaged_beers + noimage_beers
+  end
+end
