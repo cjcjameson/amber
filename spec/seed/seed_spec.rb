4000b3c031fda9695c42baeab50316acf056dd4e
djohnson0610 <djohnson0610@hotmail.com>
2014-07-08 22:34:12 -0700

    Spec written to check the seed genres method.
    When passing a json formatted string similar to that in our beer_genre.json file,
    we expect a BeerGenre object to be created

--- spec/seed/seed_spec.rb
+++ spec/seed/seed_spec.rb
@@ -1,6 +1,16 @@
 require 'spec_helper'
 
 describe Seed do
+	context "#genres" do
+		
+		it "creates BeerGenre objects when passed a valid json formatted string" do
+			genre_string = '{"beer_genres":
+	      [{"name": "Cream Ale",
+	      "description": "Smooth malty sweetness and balanced bitterness. Pairs perfectly with sweet, hot, or spicy foods including many Asian dishes, chili, mango or jalapeño salsa."}]}'
+			expect{Seed.genres(genre_string)}.to change{BeerGenre.count}.by(1)
+		end
+	end
+
 
 	context "#parse_beer_data" do
 		it "returns an array when given a json format string" do
