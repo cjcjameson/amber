c00808ca926b1f3356164f002a491f8e386e30c7
rubberyuzu <yuzu0125@gmail.com>
2014-07-08 21:00:41 -0700

    added abv %

--- app/assets/javascripts/views/view.js
+++ app/assets/javascripts/views/view.js
@@ -7,7 +7,7 @@ View.prototype = {
 	},
 	drawBeer: function(beer){
 			var sampleBeer = $('#beer_template').children().clone()
-			sampleBeer.append("<img src='"+beer.label_url+"'><h3>"+beer.name+"</h3><h5>"+beer.abv+"</h5><h5>"+beer.style+"</h5><br><p>"+beer.description+"</p>")
+			sampleBeer.append("<img src='"+beer.label_url+"' class='float_left'><div class='float_left'><h3>"+beer.name+"</h3><h5>"+beer.abv+"%</h5><h5>"+beer.style+"</h5><br><p>"+beer.description+"</p></div>")
 			$("#beer_results").append(sampleBeer)
 	}
 }
