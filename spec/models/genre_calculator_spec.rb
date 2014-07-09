f29c3700b79979edd3921ccbf0f19e58813eabb0
djohnson0610 <djohnson0610@hotmail.com>
2014-07-09 11:03:58 -0700

    Began refactoring GenreCalculator spec

--- spec/models/genre_calculator_spec.rb
+++ spec/models/genre_calculator_spec.rb
@@ -3,21 +3,42 @@ require 'spec_helper'
 describe GenreCalculator do
 	before (:each) do
 		spicy = FactoryGirl.create(:food_flavor)
-		ipa = FactoryGirl.create(:beer_genre)
-		match = Match.create(food_flavor: spicy, beer_genre: ipa)
+		pale = FactoryGirl.create(:beer_genre)
+		match = Match.create(food_flavor: spicy, beer_genre: pale)
+
+
+		sweet = FactoryGirl.create(:food_flavor, name: "sweet")
+		ipa = FactoryGirl.create(:beer_genre, name: "ipa")
+		match = Match.create(food_flavor: sweet, beer_genre: ipa)
 	end
 
-	context "#get_best_genre_matches" do
-		it "responds with an array of BeerGenres when given a list of flavors" do
-			expect(GenreCalculator.new(["spicy"]).get_best_genre_matches.first).to be_instance_of(BeerGenre)
+	describe "#get_best_genre_matches" do
+		describe "spicy GenreCalculator" do
+			let(:spicy_calc) { GenreCalculator.new(["spicy"]).get_best_genre_matches }
+			it "responds with a single match" do
+				expect(spicy_calc.length).to eq 1
+			end
+			it "responds with an array of BeerGenres when given a list of flavors" do
+				expect(spicy_calc.first).to be_instance_of(BeerGenre)
+			end
+		end
+
+		describe "spicy and sweet GenreCalculator" do
+			let(:spicy_sweet_calc) { GenreCalculator.new(["spicy", "sweet"]).get_best_genre_matches }
+			it "responds with two matches" do
+				expect(spicy_sweet_calc.length).to eq 2
+			end
+			it "responds with a collection of only BeerGenres when given a list of flavors" do
+				expect(spicy_sweet_calc.reject { |genre| genre.class == BeerGenre }).to be_empty
+			end
 		end
 
 		it "responds with an array of BeerGenres when given a list containing at least one valid flavor" do
-			expect(GenreCalculator.new(["foo", "spicy", "bar"]).get_best_genre_matches.first).to be_instance_of(BeerGenre)
+			expect(GenreCalculator.new(["foo", "spicy", "bar"]).get_best_genre_matches.length).to eq 1
 		end
 
 		it "responds with an empty array when given a list of invalid flavors" do
-			expect(GenreCalculator.new(["foo"]).get_best_genre_matches).to eq([])
+			expect(GenreCalculator.new(["foo"]).get_best_genre_matches).to be_empty
 		end
 	end
 end
