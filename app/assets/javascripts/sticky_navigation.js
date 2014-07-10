9c348565065106c67372efa646919a5743ccb5e3
allison <azywong@gmail.com>
2014-07-10 10:20:03 -0700

    removed duplicate

--- app/assets/javascripts/sticky_navigation.js
+++
@@ -1,9 +0,0 @@
-var sticky_navigation_offset_top = $('nav').offset().top
-var sticky_navigation = function(){
-  var scroll_top = $(window).scrollTop()
-  if (scroll_top > sticky_navigation_offset_top) {
-      $('nav').css({ 'position': 'fixed', 'top':0, 'left':0 })
-  } else {
-      $('nav').css({ 'position': 'relative' })
-  }
-}
