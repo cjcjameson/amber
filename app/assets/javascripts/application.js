5942949cd2df8fc45bfa78d3dcd44d022a950ecb
rubberyuzu <yuzu0125@gmail.com>
2014-07-09 19:33:03 -0700

    restructured js drastically into mvcoo

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,6 @@
 //= require jquery_ujs
 //= require_self
 //= require initialize.js
+//= require namespace.js
 //= require_tree .
 
--- app/assets/javascripts/initialize.js
+++ app/assets/javascripts/initialize.js
@@ -1,5 +1,5 @@
 $(function(){
-  beersController = new BeersController(new View())
+  beersController = new Beer.Controller(new Beer.View())
   $('#search_results').on('click', '.beer_map_button', showZipForm)
   $('#beer_results').on('click', '.zip_code_button', yelpCall)
 })
