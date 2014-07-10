5bf6b305ffca25315272d70895a71992b6e7d668
rubberyuzu <yuzu0125@gmail.com>
2014-07-09 20:10:52 -0700

    commented out back_button too

--- app/assets/javascripts/initialize.js
+++ app/assets/javascripts/initialize.js
@@ -2,10 +2,11 @@ $(function(){
   beersController = new Beers.Controller(new Beers.View())
   $('#search_results').on('click', '.beer_map_button', showZipForm)
   $('#beer_results').on('click', '.zip_code_button', yelpCall)
-  $('.back_button').click(function(){
-     $('html,body').animate({ scrollTop: $($(this).attr("href")).offset().top }, 'slow','swing');
-     return false;
-     })
+  
+  // $('.back_button').click(function(){
+  //    $('html,body').animate({ scrollTop: $($(this).attr("href")).offset().top }, 'slow','swing');
+  //    return false;
+  //    })
   // $('#beer_results img').hide();
 })
  
