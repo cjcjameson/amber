7bb0fbc79b01818017affefbf950138d7153ad8a
rubberyuzu <yuzu0125@gmail.com>
2014-07-09 17:36:20 -0700

    working on loading wheels, not working yet

--- app/assets/javascripts/initialize.js
+++ app/assets/javascripts/initialize.js
@@ -6,4 +6,20 @@ $(function(){
      $('html,body').animate({ scrollTop: $($(this).attr("href")).offset().top }, 'slow','swing');
      return false;
      })
+  $('#beer_results img').hide();
 })
+ 
+var i = 0;
+var int=0;
+$(window).bind("load", function() {
+     var int = setInterval("doThis(i)",500);
+});
+ 
+function doThis() {
+     var images = $('img').length;
+     if (i >= images) {
+          clearInterval(int);
+    }
+    $('img:hidden').eq(0).fadeIn(500);
+    i++;
+  }
