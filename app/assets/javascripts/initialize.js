6733e7252aa677725728375bb8a43f25d5497826
rubberyuzu <yuzu0125@gmail.com>
2014-07-09 20:41:19 -0700

    WIP, created Maps.Controller, to be MVCfied

--- app/assets/javascripts/initialize.js
+++ app/assets/javascripts/initialize.js
@@ -1,8 +1,7 @@
 $(function(){
   beersController = new Beers.Controller(new Beers.View())
-  $('#search_results').on('click', '.beer_map_button', showZipForm)
-  $('#beer_results').on('click', '.zip_code_button', yelpCall)
-  
+  mapController = new Maps.Controller(new Maps.View())
+
   // $('.back_button').click(function(){
   //    $('html,body').animate({ scrollTop: $($(this).attr("href")).offset().top }, 'slow','swing');
   //    return false;
--- app/assets/javascripts/namespace.js
+++ app/assets/javascripts/namespace.js
@@ -1,2 +1,2 @@
 Beers = {}
-Map = {}
+Maps = {}
