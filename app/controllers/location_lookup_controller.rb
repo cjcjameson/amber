64321e7b9730a134e88548666f7cd5af3175c543
allison <azywong@gmail.com>
2014-07-09 19:45:13 -0700

    refactored location controller
    Created OauthWrapper class that handles oauth
    Probably still can be refactored but its a start

--- app/controllers/location_lookup_controller.rb
+++ app/controllers/location_lookup_controller.rb
@@ -2,16 +2,9 @@ class LocationLookupController < ApplicationController
   respond_to :json
 
   def result
-    consumer_key = ENV["yelp_consumer_key"] || CONFIG["yelp"]["consumer_key"]
-    consumer_secret = ENV["yelp_consumer_secret"] || CONFIG["yelp"]["consumer_secret"]
-    token = ENV["yelp_token"] || CONFIG["yelp"]["token"]
-    token_secret = ENV["yelp_token_secret"] || CONFIG["yelp"]["token_secret"]
     api_host = 'api.yelp.com'
-    consumer = OAuth::Consumer.new(consumer_key, consumer_secret, {:site => "http://#{api_host}"})
-    access_token = OAuth::AccessToken.new(consumer, token, token_secret)
-    search_term = params[:genre].gsub(' ', '+')
-    path = "/v2/search?term=#{search_term}+beer&location=#{params[:zipcode]}"
-    response = JSON.parse(access_token.get(path).body)
+    oauth = OauthWrapper.new(params)
+    response = JSON.parse(oauth.yelp_it(api_host).body)
 
     if response
         latitude = response["region"]["center"]["latitude"]
