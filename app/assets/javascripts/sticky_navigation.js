bfd3712d75a9157d04227d80598668e3aa097d6e
allison <azywong@gmail.com>
2014-07-10 10:41:26 -0700

    renamed function to be more clear

--- app/assets/javascripts/sticky_navigation.js
+++ app/assets/javascripts/sticky_navigation.js
@@ -1,6 +1,6 @@
 var stickyNavigation = function(){
   var nav_offset_top = $('nav').offset().top
-  var fixNavigationPosition = function(){
+  var fixNavPosition = function(){
     var scroll_top = $(window).scrollTop()
     if (scroll_top > nav_offset_top) {
         $('nav').css({ 'position': 'fixed', 'top':0, 'left':0 })
@@ -8,8 +8,8 @@ var stickyNavigation = function(){
         $('nav').css({ 'position': 'relative' })
     }
   }
-  fixNavigationPosition()
+  fixNavPosition()
   $(window).scroll(function() {
-         fixNavigationPosition()
+         fixNavPosition()
     })
 }
