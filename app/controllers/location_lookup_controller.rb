207eea95447f048911aeb8d44707d0867aa75e32
allison <azywong@gmail.com>
2014-07-09 20:08:50 -0700

    refactored controller to use format response method

--- app/controllers/location_lookup_controller.rb
+++ app/controllers/location_lookup_controller.rb
@@ -4,16 +4,20 @@ class LocationLookupController < ApplicationController
   def result
     api_host = 'api.yelp.com'
     oauth = OauthWrapper.new(params)
-    response = JSON.parse(oauth.yelp_it(api_host).body)
+    parsed_json = JSON.parse(oauth.yelp_it(api_host).body)
 
-    if response
-        latitude = response["region"]["center"]["latitude"]
-        longitude = response["region"]["center"]["longitude"]
-        response_data = response["businesses"]
-        response = {latitude: latitude, longitude: longitude, data: response_data}
-        respond_with response
+    if parsed_json
+        respond_with format_response(parsed_json)
     else
         render status: 400
     end
+  end
+
+  private
+  def format_response(parsed_json)
+    latitude = parsed_json["region"]["center"]["latitude"]
+    longitude = parsed_json["region"]["center"]["longitude"]
+    parsed_json_data = parsed_json["businesses"]
+    response = {latitude: latitude, longitude: longitude, data: parsed_json_data}
   end
 end
